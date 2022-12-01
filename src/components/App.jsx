a9036beca9bc8e2b0c48af03889e68acb9430461
Yurii-Kavuza <kavuzaiurii@gmail.com>
2022-12-01 17:39:56 +0200

    fix empty local storage after reload page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import Section from './Section';
@@ -6,86 +6,67 @@ import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 
-class App extends React.Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      })
-    ),
-    filter: PropTypes.string,
-  };
+const LS_KEY = 'contacts';
 
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  const contactsLS = JSON.parse(localStorage.getItem(LS_KEY));
 
-  deleteContact = dataId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== dataId),
-    }));
-  };
+  const [contacts, setContacts] = useState(contactsLS ?? []);
+  const [filter, setFilter] = useState('');
 
-  formSubmitHandler = data => {
-    data.id = nanoid();
-    const contacts = this.state.contacts;
-    const containsNamee = contacts.find(contact => contact.name === data.name);
-    if (containsNamee) {
-      return alert(`${data.name} is already in contacts.`);
-    }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, data],
-    }));
-  };
-
-  filteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const filteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  onFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const deleteContact = dataId => {
+    setContacts(contacts.filter(contact => contact.id !== dataId));
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  const onFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+  const formSubmitHandler = data => {
+    data.id = nanoid();
+    const containsNamee = contacts.find(contact => contact.name === data.name);
+    if (containsNamee) {
+      return alert(`${data.name} is already in contacts.`);
     }
-  }
+    setContacts([...contacts, data]);
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-  render() {
-    const { filter } = this.state;
-    return (
-      <div>
-        <Section title={'Phonebook'}>
-          <ContactForm onSubmit={this.formSubmitHandler} />
-        </Section>
-        <Section title={'Contacts'}>
-          <Filter onFilter={this.onFilter} value={filter} />
-          <ContactList
-            contacts={this.filteredContacts()}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Section title={'Phonebook'}>
+        <ContactForm onSubmit={formSubmitHandler} />
+      </Section>
+      <Section title={'Contacts'}>
+        <Filter onFilter={onFilter} value={filter} />
+        <ContactList
+          contacts={filteredContacts()}
+          onDeleteContact={deleteContact}
+        />
+      </Section>
+    </div>
+  );
+};
+
+App.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  filter: PropTypes.string,
+};
 
 export default App;
