9517aa326e20858942852d6839c2c27c64098b7e
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-03-10 13:54:42 +0200

    small fixes

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -6,5 +6,5 @@ export const Form = styled.form`
 export const Label = styled.label`
   display: flex;
   flex-direction: column;
-  margin-bottom: ${p => p.theme.space[4]}px;
+  margin-bottom: 16px;
 `;
