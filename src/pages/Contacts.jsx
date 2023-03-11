5cc0f0506e9b80f7c9b61d2d3d3a523ea5089aa6
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-03-11 22:51:42 +0200

    delete unused imports

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { selectLoading } from 'redux/contacts/selectors';
 import { fetchContacts } from 'redux/contacts/operations';
 import Section from 'components/Section';
 import ContactForm from 'components/ContactForm';
